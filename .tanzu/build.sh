9904a53c4016b640b75ad980e8e42d7fc43d125c
bk <bryank1@vmware.com>
2022-07-13 11:23:34 -0500

    testing sourcing pom.properties to generate json file

--- .tanzu/build.sh
+++ .tanzu/build.sh
@@ -6,9 +6,11 @@
   --update-snapshots \
   --no-transfer-progress
 
-groupId=$(./mvnw help:evaluate -Dexpression=project.groupId -DforceStdout -q)
-artifactId=$(./mvnw help:evaluate -Dexpression=project.artifactId -DforceStdout -q)
-version=$(./mvnw help:evaluate -Dexpression=project.version -DforceStdout -q)
+# groupId=$(./mvnw help:evaluate -Dexpression=project.groupId -DforceStdout -q)
+# artifactId=$(./mvnw help:evaluate -Dexpression=project.artifactId -DforceStdout -q)
+# version=$(./mvnw help:evaluate -Dexpression=project.version -DforceStdout -q)
+
+source target/maven-archiver/pom.properties
 
 cat <<EOF > metadata.json
 {
