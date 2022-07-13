f0ca03edf1eae49a0385b63c55d587c1a6b13b37
bk <bryank1@vmware.com>
2022-07-13 11:14:55 -0500

    Using mvnw to pull out groupid, artifactid and version

--- .tanzu/build.sh
+++ .tanzu/build.sh
@@ -6,4 +6,16 @@
   --update-snapshots \
   --no-transfer-progress
 
-xmllint --xpath '/*[local-name()="project"]/*[local-name()="version"]/text()' pom.xml
+groupId=$(./mvnw help:evaluate -f pom.xml --offline -Dexpression=project.groupId -DforceStdout -q)
+artifactId=$(./mvnw help:evaluate -f pom.xml --offline -Dexpression=project.artifactId -DforceStdout -q)
+version=$(./mvnw help:evaluate -f pom.xml --offline -Dexpression=project.version -DforceStdout -q)
+
+cat <<EOF > metadata.json
+{
+  "groupId": "${groupId}",
+  "artifactId": "${artifactId}",
+  "version": "${version}"
+}
+EOF
+
+cat metadata.json
